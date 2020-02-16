4e1f13058845bbfda632099202c398bf6d63273f
John Paul Lee <johnpaullee@Johns-MacBook-Pro.local>
2020-02-16 18:16:32 +0000

    Week 2 Problem Sheet. Origionally completed on 26 Jan but to a different respority

---
+++ bmi.py
@@ -0,0 +1,8 @@
+#Homework for Week 2 of Programming and Scripting
+
+weight = float(input("Enter Weight in Kilograms"))
+height = float(input("Enter Height in Centimeters"))
+
+BMI = weight / (height/100)**2
+
+print("Your BMI is", BMI)
