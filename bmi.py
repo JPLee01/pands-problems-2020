215ffac50055f1d2c0105323036991943c222b1b
John Paul Lee <johnpaullee@Johns-MacBook-Pro.local>
2020-02-16 18:22:07 +0000

    Comments related to code calculations entered.

--- bmi.py
+++ bmi.py
@@ -1,8 +1,14 @@
-#Homework for Week 2 of Programming and Scripting
+#John Paul Lee
+#Problem Sheet for Week 2 of Programming and Scripting
 
+#Ask user to input their weight in kilograms
 weight = float(input("Enter Weight in Kilograms"))
+
+#ASk user to input thier height in centimeters
 height = float(input("Enter Height in Centimeters"))
 
+#BMI calaculated by dividing weight by height (in meters squared)
 BMI = weight / (height/100)**2
 
+#Print the result
 print("Your BMI is", BMI)
