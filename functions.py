3ab8b61929453b8718f3b830f547a50760b26041
John Paul Lee <johnpaullee@Johns-MacBook-Pro.local>
2020-03-17 17:24:06 +0000

    Squareroot function worked out but still struggling to be able to import it from functions.py to squareroot.py

--- functions.py
+++ functions.py
@@ -1,15 +1,63 @@
 #John Paul Lee
 #Created functions for Problem Sheet of Programming and Scripting
 
+import math
 
-x = float(input("Please enter a positive number: "))
+# Function which that returns square root of a number with precision upto 5 decimal places 
+def Square(n, i, j): 
+# 3 inputs; n = user input, i = iteration starting = 1, j = iteration also starting = 1
+    mid = (i + j) / 2; 
+    mul = mid * mid; 
+    # If mid itself is the square root, 
+    # return mid 
+    if ((mul == n) or (abs(mul - n) < 0.00001)): 
+        return mid; 
+    # If mul is less than n, recur second half 
+    elif (mul < n): 
+        return Square(n, mid, j); 
+    # Else recur first half 
+    else: 
+        return Square(n, i, mid); 
+# Function to find the square root of n 
+def findSqrt(n): 
+    i = 1; 
+    # While the square root is not found 
+    found = False; 
+    while (found == False): 
+        # If n is a perfect square 
+        if (i * i == n): 
+            print("The square root of", n, "is approx.", i); 
+            found = True; 
+        elif (i * i > n): 
+            # Square root will lie in the 
+            # interval i-1 and i 
+            res = Square(n, i - 1, i); 
+            print ("The square root of", n, "is approx.", "{0:.5f}".format(res))  
+            found = True
+        i += 1; 
+# Driver code for program 
+if __name__ == '__main__': 
+    findSqrt(n); 
 
-def sqrt (n):
-    #Find the square root of the input
-    y =  x ** (1/2)
+#The approach to the above is as follows:
+#   Start iterating from i = 1. If i * i = n, then print i as n is a perfect square whose square root is i.
+#   Else find the smallest i for which i * i is strictly greater than n.
+#   Now we know square root of n lies in the interval i – 1 and i and we can use Binary Search algorithm to find the square root.
+#   Find mid of i – 1 and i and compare mid * mid with n, with precision upto 5 decimal places.
+#       If mid * mid = n then return mid.
+#       If mid * mid < n then recur for the second half.
+#       If mid * mid > n then recur for the first half.   
 
+# References
+# "Defining and Using Functions" section of A Whirlwind Tour of Python by Jake Vanderplas.
+# https://www.geeksforgeeks.org/square-root-of-a-number-without-using-sqrt-function/
 
+#Mathematical formula for square root
+# def sqrt (n, x, x1, x2, x3, x4):
+        #x1 = [((x + (x/x)) / 2]
+            #x2  = [(x1 + (x/x1)) / 2]
+                #x3  = [(x2 + (x/x2)) / 2]
+                    #x4 = [(x3 + (x/x3)) / 2]
+                        #n = (x1 + x2 + x3 + x4)
+    #return n
 
-
-
-print("The square root of", x, "is approx.", y)
--- squareroot.py
+++ squareroot.py
@@ -1,12 +1,56 @@
 #John Paul Lee
 #Problem Sheet for Week 6 of Programming and Scripting
-
-from functions import sqrt
+import math 
 
 #ask user to input a positive floating-point number 
-x = float(input("Please enter a positive number: "))
+n = float(input("Please enter a positive number: "))
 
-y = sqrt#####error
+# Function which that returns square root of a number with precision upto 5 decimal places 
+def Square(n, i, j): 
+# 3 inputs; n = user input, i = iteration starting = 1, j = iteration also starting = 1
+    mid = (i + j) / 2; 
+    mul = mid * mid; 
+    # If mid itself is the square root, 
+    # return mid 
+    if ((mul == n) or (abs(mul - n) < 0.00001)): 
+        return mid; 
+    # If mul is less than n, recur second half 
+    elif (mul < n): 
+        return Square(n, mid, j); 
+    # Else recur first half 
+    else: 
+        return Square(n, i, mid); 
+# Function to find the square root of n 
+def findSqrt(n): 
+    i = 1; 
+    # While the square root is not found 
+    found = False; 
+    while (found == False): 
+        # If n is a perfect square 
+        if (i * i == n): 
+            print("The square root of", n, "is approx.", i); 
+            found = True; 
+        elif (i * i > n): 
+            # Square root will lie in the 
+            # interval i-1 and i 
+            res = Square(n, i - 1, i); 
+            print ("The square root of", n, "is approx.", "{0:.5f}".format(res))  
+            found = True
+        i += 1; 
+# Driver code for program 
+if __name__ == '__main__': 
+    findSqrt(n); 
 
-#print the result of the squareroot function
-print("The square root of", x, "is approx.", y)
+#The approach to the above is as follows:
+#   Start iterating from i = 1. If i * i = n, then print i as n is a perfect square whose square root is i.
+#   Else find the smallest i for which i * i is strictly greater than n.
+#   Now we know square root of n lies in the interval i – 1 and i and we can use Binary Search algorithm to find the square root.
+#   Find mid of i – 1 and i and compare mid * mid with n, with precision upto 5 decimal places.
+#       If mid * mid = n then return mid.
+#       If mid * mid < n then recur for the second half.
+#       If mid * mid > n then recur for the first half.   
+
+# References
+# "Defining and Using Functions" section of A Whirlwind Tour of Python by Jake Vanderplas.
+# https://www.geeksforgeeks.org/square-root-of-a-number-without-using-sqrt-function/
+ 
